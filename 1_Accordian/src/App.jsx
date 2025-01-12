d831dfe78fac8ed854abccd2d2be3aa24eeb5158
ExploreInsight <chiragchandel1016@gmail.com>
2025-01-12 22:03:36 +0530

    First Project Added

---
+++ 1_Accordian/src/App.jsx
@@ -0,0 +1,15 @@
+// import "./App.css";
+import Accordian from './components/Accordian.jsx';
+function App() {
+  return (
+    <>
+      <div className="app">
+      <Accordian />
+      </div>
+        
+    
+    </>
+  );
+}
+
+export default App;

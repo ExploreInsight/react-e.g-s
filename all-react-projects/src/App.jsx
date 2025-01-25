294ba8496d7f38832e619f89b3182e3b5eeed533
ExploreInsight <chiragchandel1016@gmail.com>
2025-01-25 23:34:30 +0530

    fixed app jsx

--- all-react-projects/src/App.jsx
+++ all-react-projects/src/App.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Star from "./Components/star-rating/Star";
-import './App.css'
+import "./App.css";
 import Accordion from "./Components/accordion/Accordion";
 import Color from "./Components/color-genrertor/Color";
 export default function App() {
   return (
     <div>
-      {/* Star component */}
-      {/* <Star allStar={5} /> */}
+      {/* 1.Accordion component  */}
+      <Accordion />
 
-      {/* Accordion component  */}
-      <Accordion/>
+      {/* 2.color component */}
+      <Color />
 
-      {/* color component */}
-      {/* <Color/> */}
+      {/* 3.Star component */}
+      <Star allStar={5} />
     </div>
   );
 }
